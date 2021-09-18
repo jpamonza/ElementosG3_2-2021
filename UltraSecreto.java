4812d4c252309329edea3370595f5247ec695042
Juan Pablo Amonzabel <jpamonzabel@gmail.com>
2021-09-18 19:25:16 -0400

    Agregado metodo ingresar con respuesta provisional

--- UltraSecreto.java
+++ UltraSecreto.java
@@ -1,26 +1,5 @@
-public class UltraSecreto // extends Edificio
-{
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class UltraSecreto
-     */
-    public UltraSecreto()
-    {
-        // initialise instance variables
-        x = 0;
-    }
-
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+public class UltraSecreto extends Edificio{
+    public boolean ingresar(Personal p){
+        return false;
     }
 }
