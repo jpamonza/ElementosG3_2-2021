c43af777f309619fbcf846347a8cd0da7c92f8d3
Juan Pablo Amonzabel <jpamonzabel@gmail.com>
2021-10-14 15:17:35 -0400

    Busqueda Binaria

--- Recursividad.java
+++ Recursividad.java
@@ -1,10 +1,12 @@
+import java.util.Arrays;
+
 public class Recursividad{
     /**
      * Ejercicio 14
      */
     public boolean todosPares(int n){
         boolean res;
-        
+
         if(n == 0){
             res = true;
         }else{
@@ -16,7 +18,48 @@ public class Recursividad{
                 res = false;
             }
         }
+
+        return res;
+    }
+    
+    public boolean busquedaBinaria(int[] arreglo, int x){
+        /**
+         * Algoritmo burbuja
+         * Merge Sort (Ordenamiento por mezcla)
+         * 
+         * Arreglos -> Paso de referencia
+         * Tipos primitivos -> Copia de valor
+         */
+        Arrays.sort(arreglo);
+        return busquedaBinaria(arreglo, x, 0, arreglo.length-1);
+    }
+    
+    private boolean busquedaBinaria(int[] arreglo, int x, int l, int r){
+        boolean res;
+        
+        /*int l, r, m;
+        l = 0;
+        r = arreglo.length - 1;*/
+        int m = (r + l) / 2;
+        
+        if(arreglo[m] == x){
+            res = true;
+        }else if(l == r){
+            res = false;
+        }else{
+            if(x > arreglo[m]){
+                res = busquedaBinaria(arreglo, x, m+1, r);
+            }else{
+                res = busquedaBinaria(arreglo, x, l, m);
+            }
+        }
         
         return res;
     }
 }
+
+
+
+
+
+
